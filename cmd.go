36b946cf163d1b312cb74572bdde0e1b6f642255
Karan Misra <karan.misra@go-jek.com>
2019-05-18 23:37:36 +0530

    Extract commands

--- cmd.go
+++ cmd.go
@@ -10,11 +10,12 @@ import (
 	"github.com/urfave/cli"
 )
 
-var version = "0.2.4"
+var (
+	version = "0.2.4"
+	d       *data
+)
 
 func main() {
-	var d *data
-
 	app := cli.NewApp()
 	app.Name = "xp"
 	app.HelpName = "xp"
@@ -72,139 +73,151 @@ func main() {
 	}
 
 	app.Commands = []cli.Command{
-		{
-			Name:    "show-config",
-			Aliases: []string{"sc"},
-			Usage:   "Print the current config",
-			Action: func(c *cli.Context) error {
-				fmt.Print(d)
-				return nil
-			},
-		},
-		{
-			Name:        "add-info",
-			Usage:       "Add xp info to the COMMIT msg file",
-			Description: "This is supposed to be invoked from inside a prepare-commit-msg hook",
-			ArgsUsage:   "commit-msg-file",
-			Action: func(c *cli.Context) error {
-				wd, err := os.Getwd()
-				if err != nil {
-					return errors.Wrap(err, "could not get wd")
-				}
-
-				msgFile := c.Args().Get(0)
-
-				if err := d.appendInfo(wd, msgFile); err != nil {
-					return errors.Wrap(err, "info add failed")
-				}
-
-				return nil
-			},
-		},
-		{
-			Name:    "dev",
-			Aliases: []string{"d"},
-			Usage:   "Dev management",
-			Subcommands: []cli.Command{
-				{
-					Name:      "add",
-					Aliases:   []string{"a"},
-					Usage:     "Add a new developer",
-					ArgsUsage: `id "name" email`,
-					Action: func(c *cli.Context) error {
-						args := c.Args()
-
-						id, name, email := args.Get(0), args.Get(1), args.Get(2)
-						if id == "" || name == "" || email == "" {
-							return errors.New("invalid id/name/email")
-						}
-
-						d.addDev(id, name, email)
-						return nil
-					},
-				},
-			},
-		},
-		{
-			Name:    "repo",
-			Aliases: []string{"r"},
-			Usage:   "Repo management",
-			Subcommands: []cli.Command{
-				{
-					Name:      "init",
-					Aliases:   []string{"i"},
-					Usage:     "Initialize a repo. Setup prepare-commit-msg hook",
-					ArgsUsage: ".",
-					Flags: []cli.Flag{
-						cli.BoolFlag{
-							Name:  "overwrite",
-							Usage: "overwrite the .git/hooks/prepare-commit-msg file",
-						},
-						cli.StringSliceFlag{
-							Name:  "devs",
-							Usage: "initial set of devs (optional)",
-						},
-						cli.StringFlag{
-							Name:  "story-id",
-							Usage: "story id (optional)",
-						},
-					},
-					Action: func(c *cli.Context) error {
-						args := c.Args()
-
-						dir := args.Get(0)
-						if dir == "." || dir == "" {
-							var err error
-							dir, err = os.Getwd()
-							if err != nil {
-								return errors.Wrap(err, "could not get wd")
-							}
-						}
-
-						xpBinPath, err := os.Executable()
-						if err != nil {
-							return errors.Wrap(err, "xp binary path resolve failed")
-						}
-
-						overwrite := c.Bool("overwrite")
-						if err := initRepo(dir, overwrite, xpBinPath); err != nil {
-							return errors.Wrap(err, "repo .git hook init failed")
-						}
-
-						devs := c.StringSlice("devs")
-						storyID := c.String("story-id")
-
-						if err := d.addRepo(dir, devs, storyID); err != nil {
-							return errors.Wrap(err, "could add init repo")
-						}
-
-						return nil
-					},
-				},
-				{
-					Name:      "devs",
-					Aliases:   []string{"d", "dev"},
-					Usage:     "Set list of devs working on repo",
-					ArgsUsage: "dev1 dev2 dev3",
-					Action: func(c *cli.Context) error {
-						wd, err := os.Getwd()
-						if err != nil {
-							return errors.Wrap(err, "could not get wd")
-						}
-						devs := c.Args()
-
-						if err := d.updateRepoDevs(wd, devs); err != nil {
-							return errors.Wrap(err, "could not set devs")
-						}
-
-						return nil
-					},
-				},
-			},
-		},
+		showConfigCommand,
+		addInfoCommand,
+		devCommand,
+		repoCommand,
 	}
 
 	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
+}
+
+var showConfigCommand = cli.Command{
+	Name:    "show-config",
+	Aliases: []string{"sc"},
+	Usage:   "Print the current config",
+	Action: func(c *cli.Context) error {
+		fmt.Print(d)
+		return nil
+	},
+}
+
+var addInfoCommand = cli.Command{
+	Name:        "add-info",
+	Usage:       "Add xp info to the COMMIT msg file",
+	Description: "This is supposed to be invoked from inside a prepare-commit-msg hook",
+	ArgsUsage:   "commit-msg-file",
+	Action: func(c *cli.Context) error {
+		wd, err := os.Getwd()
+		if err != nil {
+			return errors.Wrap(err, "could not get wd")
+		}
+
+		msgFile := c.Args().Get(0)
+
+		if err := d.appendInfo(wd, msgFile); err != nil {
+			return errors.Wrap(err, "info add failed")
+		}
+
+		return nil
+	},
+}
+
+var devCommand = cli.Command{
+	Name:    "dev",
+	Aliases: []string{"d"},
+	Usage:   "Dev management",
+	Subcommands: []cli.Command{
+		{
+			Name:      "add",
+			Aliases:   []string{"a"},
+			Usage:     "Add a new developer",
+			ArgsUsage: `id "name" email`,
+			Action: func(c *cli.Context) error {
+				args := c.Args()
+
+				id, name, email := args.Get(0), args.Get(1), args.Get(2)
+				if id == "" || name == "" || email == "" {
+					return errors.New("invalid id/name/email")
+				}
+
+				d.addDev(id, name, email)
+				return nil
+			},
+		},
+	},
+}
+
+var repoCommand = cli.Command{
+	Name:    "repo",
+	Aliases: []string{"r"},
+	Usage:   "Repo management",
+	Subcommands: []cli.Command{
+		initCommand,
+		repoDevsCommand,
+	},
+}
+
+var initCommand = cli.Command{
+	Name:      "init",
+	Aliases:   []string{"i"},
+	Usage:     "Initialize a repo. Setup prepare-commit-msg hook",
+	ArgsUsage: ".",
+	Flags: []cli.Flag{
+		cli.BoolFlag{
+			Name:  "overwrite",
+			Usage: "overwrite the .git/hooks/prepare-commit-msg file",
+		},
+		cli.StringSliceFlag{
+			Name:  "devs",
+			Usage: "initial set of devs (optional)",
+		},
+		cli.StringFlag{
+			Name:  "story-id",
+			Usage: "story id (optional)",
+		},
+	},
+	Action: func(c *cli.Context) error {
+		args := c.Args()
+
+		dir := args.Get(0)
+		if dir == "." || dir == "" {
+			var err error
+			dir, err = os.Getwd()
+			if err != nil {
+				return errors.Wrap(err, "could not get wd")
+			}
+		}
+
+		xpBinPath, err := os.Executable()
+		if err != nil {
+			return errors.Wrap(err, "xp binary path resolve failed")
+		}
+
+		overwrite := c.Bool("overwrite")
+		if err := initRepo(dir, overwrite, xpBinPath); err != nil {
+			return errors.Wrap(err, "repo .git hook init failed")
+		}
+
+		devs := c.StringSlice("devs")
+		storyID := c.String("story-id")
+
+		if err := d.addRepo(dir, devs, storyID); err != nil {
+			return errors.Wrap(err, "could add init repo")
+		}
+
+		return nil
+	},
+}
+
+var repoDevsCommand = cli.Command{
+	Name:      "devs",
+	Aliases:   []string{"d", "dev"},
+	Usage:     "Set list of devs working on repo",
+	ArgsUsage: "dev1 dev2 dev3",
+	Action: func(c *cli.Context) error {
+		wd, err := os.Getwd()
+		if err != nil {
+			return errors.Wrap(err, "could not get wd")
+		}
+		devs := c.Args()
+
+		if err := d.updateRepoDevs(wd, devs); err != nil {
+			return errors.Wrap(err, "could not set devs")
+		}
+
+		return nil
+	},
 }
